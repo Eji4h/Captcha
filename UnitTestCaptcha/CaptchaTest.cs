1bc5ed25b266966444fbe0a00a91ba73437838fb
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2015-05-10 00:12:28 +0700

    Refactor test

--- UnitTestCaptcha/CaptchaTest.cs
+++ UnitTestCaptcha/CaptchaTest.cs
@@ -9,17 +9,21 @@ namespace UnitTestCaptcha
     {
         _Captcha captcha;
 
+        int patternDummy = 1,
+            operatorValueDummy = 1,
+            rightOperandDummy = 1;
+
         [Test]
         public void GetLeftOperand_ShouldBe1_WhenInputIs1()
         {
-            captcha = new _Captcha(1, 1, 1, 1);
+            captcha = new _Captcha(patternDummy, 1, operatorValueDummy, rightOperandDummy);
             Assert.AreEqual("1", captcha.GetLeftOperand());
         }
 
         [Test]
         public void GetLeftOperand_ShouldBe2_WhenInputIs2()
         {
-            captcha = new _Captcha(1, 2, 1, 1);
+            captcha = new _Captcha(patternDummy, 2, operatorValueDummy, rightOperandDummy);
             Assert.AreEqual("2", captcha.GetLeftOperand());
         }
     }
