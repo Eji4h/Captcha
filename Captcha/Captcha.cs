614be7c164c1fcc493a8c10613eeb4dc903be1da
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2015-05-10 00:14:31 +0700

    Refactor

--- Captcha/Captcha.cs
+++ Captcha/Captcha.cs
@@ -17,11 +17,7 @@ namespace Captcha
 
         public string GetLeftOperand()
         {
-            if (leftOperand == 9)
-                return "9";
-            if (leftOperand == 2)
-                return "2";
-            return "1";
+            return leftOperand.ToString();
         }
     }
 }
