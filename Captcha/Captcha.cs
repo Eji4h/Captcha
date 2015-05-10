2e770390656c69a727bfa544ea2e6b1c0f7c5dbc
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2015-05-11 01:53:55 +0700

    Refactor

--- Captcha/Captcha.cs
+++ Captcha/Captcha.cs
@@ -66,7 +66,7 @@ namespace Captcha
 
         public string GetCaptcha()
         {
-            return GetLeftOperand() + " " + GetOperator() + " " + GetRightOperand();
+            return string.Format("{0} {1} {2}", GetLeftOperand(), GetOperator(), GetRightOperand());
         }
     }
 }
